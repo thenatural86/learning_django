cc0f60e24523bc546c68b0a32004669eaed5c7cb
thenatural86 <philip.smalls86@gmail.com>
2024-08-15 15:27:24 -0300

    basic function based views

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render  
+from django.http.response import HttpResponse 
+
 
 # Create your views here.
+def simple_view(request):
+    return HttpResponse("Simple view")
