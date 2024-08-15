68ae0fb31532a49d8a409e24d8e17d7af3ae86d0
thenatural86 <philip.smalls86@gmail.com>
2024-08-15 12:54:26 -0300

    render first view

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # type: ignore
+from django.urls import path,include # type: ignore
 
 urlpatterns = [
+    path('my_app/',include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
