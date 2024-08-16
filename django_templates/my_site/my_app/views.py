d4a72f637af5d1902814907f18686348684a41d9
thenatural86 <philip.smalls86@gmail.com>
2024-08-16 13:17:01 -0300

    template directories and migration

---
+++ django_templates/my_site/my_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def example_view(request):
+    # my_app/templates/my_app/example.html
+    return render(request,'my_app/example.html')
