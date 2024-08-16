d3e8a8b6ad59e8729b9a1b3bee1db44e8e91248c
thenatural86 <philip.smalls86@gmail.com>
2024-08-16 10:15:33 -0300

    more on redirects and reverse

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('<int:num1>/<int:num2>', views.add_view),
     path('<int:page_number>',views.page_num_view),
     path('<int:num_page>', views.num_page_view),
-    path('<str:topic>/', views.news_view),
-    path('<int:num1>/<int:num2>', views.add_view)
+    path('<str:topic>/', views.news_view, name='topic-page')
 ]
