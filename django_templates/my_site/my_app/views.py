d66c7a50c5974f2d8402a65f58c488452c9c7399
thenatural86 <philip.smalls86@gmail.com>
2024-08-16 14:10:32 -0300

    variables in templates

--- django_templates/my_site/my_app/views.py
+++ django_templates/my_site/my_app/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 def example_view(request):
     # my_app/templates/my_app/example.html
     return render(request,'my_app/example.html')
+
+def variable_view(request):
+    
+    my_var = {
+        'first_name': 'Rosalind',
+        'last_name': 'Franklin',
+        'some_list': [1,2,3],
+        'some_dict': {'inside_key': 'inside_value'}
+    }
+    
+    
+    return render(request, 'my_app/variable.html',context=my_var)
